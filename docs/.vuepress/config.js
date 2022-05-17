3b00c398bb2b39d6ae6bdd3d140b3fbf4802336d
liuqi <boxingliuqi@163.com>
2022-05-17 22:20:52 +0800

    vssue

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,31 +7,25 @@ module.exports = {
     head: [
         ['meta', { name: 'author', content: 'vuepress介绍 lkk' }],
         ['meta', { name: 'keywords', content: 'vuepress介绍 lkk' }],
-        ['link', { rel: 'manifest', href: '/manifest.json' }],
-        ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-        ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-        ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-        ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
-        ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
-        ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
-        ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
     ],
-    plugins: [
-        [
-            '@vuepress/last-updated', {
-                transformer: (timestamp) => {
-                    return moment(timestamp).format("LLLL")
-                }
+    plugins: {
+        '@vuepress/last-updated': {
+            transformer: (timestamp) => {
+                return moment(timestamp).format("LLLL")
             }
-        ],
-        ['@vuepress/pwa', {
-            serviceWorker: true,
-            updatePopup: {
-                message: "新内容可用",
-                buttonText: "刷新"
-            }
-        }]
-    ],
+        },
+        '@vssue/vuepress-plugin-vssue': {
+            // 设置 `platform` 而不是 `api`
+            platform: 'github-v4',
+            // 其他的 Vssue 配置
+            owner: '5267lq',
+            repo: 'lkkBloc',
+            clientId: 'd3fb2b278d041f156ab0',
+            clientSecret: '4d3a43936e32f41e9a006106d4fe1d5f1d35ff07',
+            autoCreateIssue:true,
+            locale: 'zh',
+        },
+    },
     themeConfig: {
         logo: '/assets/img/tim.jpg',
         lastUpdated: '更新时间',
@@ -41,7 +35,7 @@ module.exports = {
         nav: [
             // 单个地址
             { text: '首页', link: '/' },
-            { text: '导航', link: '/guide/' },
+            { text: '导航', link: '/about.html' },
             { text: '谷歌', link: 'https://google.com' },
             // 多个地址
             {
