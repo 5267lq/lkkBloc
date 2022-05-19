a2eb79ac2a761b7a04b999e46d1fe8ff4c148c68
liuqi <boxingliuqi@163.com>
2022-05-19 21:30:45 +0800

    隐藏私密信息

--- docs/.vuepress/config/pluginsConf.js
+++ docs/.vuepress/config/pluginsConf.js
@@ -1,4 +1,5 @@
 const moment = require('moment');
+const secret=require("./secret.js");
 moment.locale("zh-cn")
 module.exports = {
     '@vuepress/last-updated': {
@@ -12,14 +13,14 @@ module.exports = {
         // 其他的 Vssue 配置
         owner: '5267lq',
         repo: 'lkkBloc',
-        clientId: 'd3fb2b278d041f156ab0',
-        clientSecret: '4d3a43936e32f41e9a006106d4fe1d5f1d35ff07',
+        clientId:secret.clientId,
+        clientSecret:secret.clientSecret,
         autoCreateIssue: true,
         locale: 'zh',
     },
     '@vuepress/back-to-top': true,
     '@vuepress/google-analytics': {
-        'ga': 'G-0683GMDTF6'
+        'ga':secret.ga
     },
     "vuepress-plugin-auto-sidebar": {
         // options
